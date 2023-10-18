6dcd546f7a216a919bf3e5b703118a1109c73db1
Serhii Maruniak <turret2009@gmail.com>
2023-10-18 20:32:41 +0300

    added auth contoller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,58 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+});
+
 exports.register = (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
+  const { name, email, password, passwordConfirm } = req.body;
+  console.log(name, email, password, passwordConfirm);
+
+  let sql = `SELECT email FROM users WHERE email = ?`;
+  db.query(
+    `SELECT email FROM users WHERE email = ?`,
+    [email],
+    async (err, resp) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (resp.length > 0) {
+        return res.render("register", {
+          message: "That email is already registred",
+        });
+      } else if (password !== passwordConfirm) {
+        return res.render("register", {
+          message: "Password aren't the same",
+        });
+      }
+
+      let hashedPassword = await bcrypt.hash(password, 8);
+      console.log(hashedPassword);
+
+      db.query(
+        `INSERT INTO users SET ?`,
+        {
+          name: name,
+          email: email,
+          password: hashedPassword,
+        },
+        (err, resp) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(resp);
+            return res.render("register", {
+              message: "User registered",
+            });
+          }
+        }
+      );
+    }
+  );
 };
